2099438f90e15c3a737aac7b3cd6f00275d9652a
neranjan-r-2187 <neranjan.r.s.134@kalvium.community>
2026-02-07 11:13:53 +0530

    07/2/26

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -5,4 +5,6 @@ const postController = require('../controllers/posts.controller.js');
 
 router.get('/', postController.getAllPosts);
 
+router.get('/:postId', postController.getPostById);
+
 module.exports = router;
